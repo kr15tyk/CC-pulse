e32a3deaa3e422ebc9241c45c71d88e69084d74f
kr15tyk <64501125+kr15tyk@users.noreply.github.com>
2026-04-20 19:24:33 -0400

    fix(config): update NSA CSfC URLs, NSA RSS, and CCDB-014 path (closes #6 partial)
    - CSFC_PAGES: all 6 sub-paths updated to new /Resources/Commercial-Solutions-for-Classified-Program/ base
    - NSA RSS: replaced dead /rss/rssfeed.aspx?POIID=9 with site-wide RSS feed
    - CCDB-014: corrected path from /files/ccfiles/ to /files/operatingprocedures/ with full filename

--- config.py
+++ config.py
@@ -185,12 +185,12 @@ BODY_WATCH_KEYWORDS = [
 CSFC_BASE = "https://www.nsa.gov"
 
 CSFC_PAGES = {
-        "home":          "/resources/everyone/csfc/",
-        "apl":           "/resources/everyone/csfc/approved-products-list/",
-        "cap_packages":  "/resources/everyone/csfc/capability-packages/",
-        "faq":           "/resources/everyone/csfc/faqs/",
-        "registration":  "/resources/everyone/csfc/registration/",
-        "kmr":           "/resources/everyone/csfc/key-management/",
+        "home":          "/Resources/Commercial-Solutions-for-Classified-Program/",
+        "apl":           "/Resources/Commercial-Solutions-for-Classified-Program/Components-List/",
+        "cap_packages":  "/Resources/Commercial-Solutions-for-Classified-Program/Capability-Packages/",
+        "faq":           "/Resources/Commercial-Solutions-for-Classified-Program/faq/",
+        "registration":  "/Resources/Commercial-Solutions-for-Classified-Program/Solution-Registration/",
+        "kmr":           "/Resources/Commercial-Solutions-for-Classified-Program/Customer-Handbook/",
         "announcements": "/Resources/Commercial-Solutions-for-Classified-Program/Announcements/",
 }
 
@@ -227,7 +227,7 @@ CSFC_COMPONENTS_LIST_URL = "https://www.nsa.gov/Resources/Commercial-Solutions-f
 CSFC_PRODUCT_LIST_URL = "https://www.nsa.gov/Resources/Commercial-Solutions-for-Classified-Program/Components-List/"
 
 CSFC_FEEDS = [
-        {"name": "NSA Cybersecurity Advisories", "rss": "https://www.nsa.gov/rss/rssfeed.aspx?POIID=9", "scrape": False},
+        {"name": "NSA Cybersecurity Advisories", "rss": "https://www.nsa.gov/DesktopModules/ArticleCS/RSS.ashx?ContentType=1&Site=1282&max=20", "scrape": False},
         {"name": "CISA Alerts",                  "rss": "https://www.cisa.gov/cybersecurity-advisories/all.xml", "scrape": False},
         {"name": "DISA STIGs & APL News",        "rss": None, "url": "https://public.cyber.mil/stigs/", "scrape": True},
 ]
